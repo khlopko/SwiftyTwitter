40bb8e55414949212284cd8fd44237f4f3ce32ff
Kirill Khlopko <k.khlopko@mobidev.biz>
2016-10-14 14:48:12 +0300

    Initial Commit

---
+++ SwiftyTwitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyTwitter
+//
+//  Created by Kirill Khlopko on 10/14/16.
+//  Copyright © 2016 Kirill Khlopko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
